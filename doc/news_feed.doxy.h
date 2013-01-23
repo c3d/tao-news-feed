a497df0d2fc1bab15be85fce1549d90b68d7cc8f
Christophe de Dinechin <christophe@taodyne.com>
2013-01-23 23:52:17 +0100

    Merge with Jerome's recent packaging changes

--- doc/news_feed.doxy.h
+++ doc/news_feed.doxy.h
@@ -1,12 +1,12 @@
 ﻿/**
  * @~english
  * @taomoduledescription{NewsFeed, News Feed}
- *
+ * <tt>import NewsFeed</tt> - Receive RSS feed.
  * @endtaomoduledescription{NewsFeed}
  *
  * @~french
  * @taomoduledescription{NewsFeed, Flux d'actualités}
- *
+ * <tt>import NewsFeed</tt> - Reçoit un flux RSS.
  * @endtaomoduledescription{NewsFeed}
  * @~
  * @{
